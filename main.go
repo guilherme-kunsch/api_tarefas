05d52eec162f968ee4e6dc58bebedbfe400b413a
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-09-17 20:09:12 -0300

    adicionando o CORS

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"tarefas/servidor"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
+
 	db, err := banco.Connection()
 	if err != nil {
 		fmt.Println("Erro ao conectar no banco", err)
@@ -19,6 +21,14 @@ func main() {
 
 	r := mux.NewRouter()
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5002"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+	handler := c.Handler(r)
+
 	r.HandleFunc("/v1/tarefas", servidor.CriarTarefas).Methods(http.MethodPost)
 	r.HandleFunc("/v1/tarefas", servidor.BuscarTarefas).Methods(http.MethodGet)
 	r.HandleFunc("/v1/tarefas/{id}", servidor.BuscarTarefa).Methods(http.MethodGet)
@@ -26,6 +36,6 @@ func main() {
 	r.HandleFunc("/v1/tarefas/{id}", servidor.DeletarTarefa).Methods(http.MethodDelete)
 
 	fmt.Print("Servidor Ligado\n")
-	http.ListenAndServe(":5001", r)
+	http.ListenAndServe(":5001", handler)
 
 }
